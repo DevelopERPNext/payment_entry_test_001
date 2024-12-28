6c39ed0709db02c91daaaaf61dcdc06b28de75e9
DevelopERPNext <develop.erpnext@gmail.com>
2024-12-28 17:56:43 +0200

    first commit 08

---
+++ payment_entry_test_001/payment_entry_test_001/doctype/item_last_invoices_test_0005/item_last_invoices_test_0005.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, khattab@info.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class item_last_invoices_test_0005(Document):
+	pass
