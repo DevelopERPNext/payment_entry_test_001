e9768b35e3ac07b09cc3e0b25b044d0e5d719717
DevelopERPNext <develop.erpnext@gmail.com>
2025-01-06 16:46:34 +0200

    first commit 09

---
+++ payment_entry_test_001/payment_entry_test_001/doctype/payment_entry_test_0003/payment_entry_test_0003.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, khattab@info.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class payment_entry_test_0003(Document):
+	pass
